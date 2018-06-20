269a0b8e2cc48f8e0e15ea71a430814224028253
Bill So <hokchun@gmail.com>
2018-06-20 23:31:38 +0800

    Initial Commit

---
+++ ManagedObjectRetainCycle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ManagedObjectRetainCycle
+//
+//  Created by Bill on 6/20/18.
+//  Copyright © 2018 Headnix. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
